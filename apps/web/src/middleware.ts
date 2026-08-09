de25a1a3671d7c0c194d8ff5e30b3b6ce2f22e4f
Ilannildo Viana <ilannildoviana12@gmail.com>
2026-08-09 15:40:29 -0300

    fix: auth

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,18 +1,14 @@
-import { HttpStatusCode } from 'axios';
 import { getSessionCookie } from 'better-auth/cookies';
 import { type NextRequest, NextResponse } from 'next/server';
 import { match } from 'path-to-regexp';
-import { API_AUTH_PREFIX, PROTECTED_ROUTES, PUBLIC_ROUTES } from './common/constants/routes';
-import { api } from './trpc/server';
-import { UserDto } from '@kikos/shared';
+import { API_AUTH_PREFIX, PUBLIC_ROUTES } from './common/constants/routes';
 
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
 
   const isAccessingApiAuthRoute = pathname.startsWith(API_AUTH_PREFIX);
   const isAccessingAuthRoute = PUBLIC_ROUTES.some((route) => match(route)(pathname));
-
-  const protectedRoute = PROTECTED_ROUTES.find((route) => match(route.route)(pathname));
+  
 
   if (isAccessingApiAuthRoute || isAccessingAuthRoute) {
     return NextResponse.next();
@@ -21,38 +17,6 @@ export async function middleware(req: NextRequest) {
   const sessionCookie = getSessionCookie(req);
   if (!sessionCookie) {
     const url = new URL('/login', req.url);
-    return NextResponse.redirect(url);
-  }
-
-  let user: UserDto | undefined;
-
-  try {
-    user = await api.users.me();
-  } catch (error: any) {
-    if (error.cause) {
-      const { status } = error.cause;
-      const isUnauthorized = status === HttpStatusCode.Unauthorized;
-
-      if (isUnauthorized) {
-        const url = new URL('/login', req.url);
-        return NextResponse.redirect(url);
-      }
-    }
-  }
-
-  if (!user) {
-    const url = new URL('/login', req.url);
-    return NextResponse.redirect(url);
-  }
-
-  if (!user && protectedRoute) {
-    const url = new URL('/login', req.url);
-    url.searchParams.set('callbackUrl', encodeURI(req.url));
-    return NextResponse.redirect(url);
-  }
-
-  if (protectedRoute && !protectedRoute.roles.some((role) => role === user.role)) {
-    const url = new URL('/forbidden', req.url);
     return NextResponse.redirect(url);
   }
 

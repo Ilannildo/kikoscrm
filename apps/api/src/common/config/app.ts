7afc6e9278e6bdbf71e9554bef611eb1c035b0ab
Ilannildo Viana <ilannildoviana12@gmail.com>
2026-08-09 15:30:56 -0300

    fix: auth

--- apps/api/src/common/config/app.ts
+++ apps/api/src/common/config/app.ts
@@ -1,7 +1,7 @@
 const ROUNDS_OF_HASHING = 10;
 const ACCESS_TTL_SECONDS = 1 * 60 * 60;
 const REFRESH_TTL_DAYS = 7;
-const DOMAIN_REFERENCE = 'teragestor.com.br';
-const CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https.teragestor.com.br', 'https://app.teragestor.com.br'];
+const DOMAIN_REFERENCE = '.kikos.teragestor.com.br';
+const CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://api.kikos.teragestor.com.br', 'https://app.kikos.teragestor.com.br'];
 
 export { ACCESS_TTL_SECONDS, CORS_ALLOWED_ORIGINS, REFRESH_TTL_DAYS, ROUNDS_OF_HASHING, DOMAIN_REFERENCE };

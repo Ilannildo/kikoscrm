92a58988fb07729e426c816b4b56938af5d940b9
Ilannildo Viana <ilannildoviana12@gmail.com>
2026-08-09 11:54:32 -0300

    fix: deploy health

--- apps/api/src/modules/health/health.controller.ts
+++ apps/api/src/modules/health/health.controller.ts
@@ -1,4 +1,3 @@
-import { SkipSubscriptionCheck } from '@common/decorators/subscription.decorator';
 import { Controller, Get } from '@nestjs/common';
 import { ApiExcludeController, ApiTags } from '@nestjs/swagger';
 import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
@@ -7,7 +6,6 @@ import { AllowAnonymous } from '@thallesp/nestjs-better-auth';
 @ApiTags('Health')
 @Controller('/health')
 @AllowAnonymous()
-@SkipSubscriptionCheck()
 export class HealthController {
 	@Get()
 	health() {

0afd080cade2d753aa54fc3f809d1b6e3abefd58
Ilannildo Viana <ilannildoviana12@gmail.com>
2026-09-18 20:43:53 -0300

    feat: fiscal spedy provider

--- apps/api/src/common/config/app.ts
+++ apps/api/src/common/config/app.ts
@@ -1,7 +1,7 @@
 const ROUNDS_OF_HASHING = 10;
 const ACCESS_TTL_SECONDS = 1 * 60 * 60;
 const REFRESH_TTL_DAYS = 7;
-const DOMAIN_REFERENCE = '.kikos.teragestor.com.br';
-const CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://api.kikos.teragestor.com.br', 'https://app.kikos.teragestor.com.br'];
+const DOMAIN_REFERENCE = '.kikos.teragestor.com';
+const CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://api.kikos.teragestor.com', 'https://app.kikos.teragestor.com'];
 
 export { ACCESS_TTL_SECONDS, CORS_ALLOWED_ORIGINS, REFRESH_TTL_DAYS, ROUNDS_OF_HASHING, DOMAIN_REFERENCE };
